dfe84d670e7578fcb5e391909d2fa05cc7455968
Surrui <Surrui@HAL9000>
2016-04-10 20:22:39 +0200

    Commit: Archivo FXML y Controller adecuados

--- Tripulacio/src/main/java/net/javierjimenez/Tripulacio/TripulacioController.java
+++ Tripulacio/src/main/java/net/javierjimenez/Tripulacio/TripulacioController.java
@@ -3,6 +3,12 @@ package net.javierjimenez.Tripulacio;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,25 +19,46 @@ import javafx.scene.control.ListView;
 public class TripulacioController implements Initializable {
 
 	@FXML
-	private ListView tripulacio;
-	
+	private ListView<String> tripulacio = new ListView<String>();
+
 	@FXML
 	private Label navegar;
-	
+
 	@FXML
 	private ComboBox<String> llistaVaixells = new ComboBox<>();
-	
+
+	private EntityManager e;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Tripulants");
+		e = emf.createEntityManager();
+
 	}
-	
-	public void generarVaixells(ActionEvent event){
-		
+
+	public void generarVaixells(ActionEvent event) {
+
+		e.getTransaction().begin();
+		// e.persist(alumne);
+		e.getTransaction().commit();
+
+		e.close();
+
 	}
-	
-	public void seleccionarVaixell(ActionEvent event){
-		
+
+	public void seleccionarVaixell(ActionEvent event) {
+
+		ObservableList<String> prueba = FXCollections.observableArrayList();
+
+		int i = 0;
+
+		while (i < 10) {
+			i++;
+			prueba.add("Prueba");
+		}
+
+		tripulacio.setItems(prueba);
+
 	}
 }
